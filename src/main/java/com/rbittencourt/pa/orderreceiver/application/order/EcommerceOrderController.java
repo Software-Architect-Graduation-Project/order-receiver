f64e5200be883e7455ff4677d7bb71c70b2c6dee
robsonbittencourt <robson.luizv@gmail.com>
2021-08-21 15:19:15 -0300

    Add time log

--- src/main/java/com/rbittencourt/pa/orderreceiver/application/order/EcommerceOrderController.java
+++ src/main/java/com/rbittencourt/pa/orderreceiver/application/order/EcommerceOrderController.java
@@ -2,6 +2,8 @@ package com.rbittencourt.pa.orderreceiver.application.order;
 
 import com.rbittencourt.pa.orderreceiver.infrastructure.ecommerceorder.EcommerceOrder;
 import com.rbittencourt.pa.orderreceiver.infrastructure.ecommerceorder.EcommerceOrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import static java.lang.System.currentTimeMillis;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @Controller
@@ -20,12 +23,23 @@ public class EcommerceOrderController {
     @Autowired
     private KafkaTemplate<String, EcommerceOrder> kafkaTemplate;
 
+    private final Logger logger = LoggerFactory.getLogger(EcommerceOrderController.class);
+
     @PostMapping
     public ResponseEntity<Object> createOrder(@RequestBody EcommerceOrderRequest request) {
         EcommerceOrder ecommerceOrder = new EcommerceOrder(request.getClientId(), request.getPaymentPlan(), request.getProducts());
-        kafkaTemplate.send("new_ecommerce_order", ecommerceOrder);
 
+        long initKafka = currentTimeMillis();
+        kafkaTemplate.send("new_ecommerce_order", ecommerceOrder);
+        long endKafka = currentTimeMillis();
+        long timeKafka = endKafka - initKafka;
+
+        long initDb = currentTimeMillis();
         repository.save(ecommerceOrder);
+        long endDb = currentTimeMillis();
+        long timeDb = endDb - initDb;
+
+        logger.info("Kafka time: " + timeKafka + " Db time: " + timeDb + " Total time: " + (timeKafka + timeDb));
 
         return new ResponseEntity<>(CREATED);
     }
